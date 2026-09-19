ee378362809732d3b25f2f66f411d6d94911488b
Sajad Bitaraf <65459976+bitarafsajad@users.noreply.github.com>
2026-09-19 17:23:43 +0330

    Update sw.js

--- previsit/sw.js
+++ previsit/sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE whenever you replace index.html, or clients keep the old copy.
-const CACHE = 'previsit-v13';
+const CACHE = 'previsit-v14';
 const SHELL = [
   './', './index.html', './manifest.webmanifest',
   './icons/icon-192.png', './icons/icon-512.png',
